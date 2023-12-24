a6baac4543be774b54a842fd58de953f4cc26c79
vivek kumar <86564710+VivekZZZ@users.noreply.github.com>
2023-12-25 00:06:55 +0530

    hashrouting done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./app.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Contact from "./components/Contact";
 import Project from "./components/Project";
 import Top from "./components/Top";
@@ -9,14 +9,14 @@ import About from "./components/About";
 const App = () => {
   return (
     <div id="app">
-      <BrowserRouter>
+      <HashRouter basename="/portfolio">
         <Routes>
           <Route path="/" element={<Top />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/project" element={<Project />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
